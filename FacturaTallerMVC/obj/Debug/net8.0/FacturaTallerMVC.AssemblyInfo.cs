fec79a8f14aa1d0f98b1046fc03d5d2c2fe90de3
avazquez <pallarete@gmail.com>
2024-10-16 17:01:03 +0200

    ffff

--- FacturaTallerMVC/obj/Debug/net8.0/FacturaTallerMVC.AssemblyInfo.cs
+++ FacturaTallerMVC/obj/Debug/net8.0/FacturaTallerMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturaTallerMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf2f8f8fd9aaf992384f13ae58a28fb8d7195c76")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturaTallerMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturaTallerMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
