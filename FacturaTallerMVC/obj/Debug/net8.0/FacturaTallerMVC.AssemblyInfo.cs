f3ef374a30ae6cc379b7efb1813ebcca010d3276
avazquez <pallarete@gmail.com>
2024-10-17 16:45:58 +0200

    ya casi que arranca

--- FacturaTallerMVC/obj/Debug/net8.0/FacturaTallerMVC.AssemblyInfo.cs
+++ FacturaTallerMVC/obj/Debug/net8.0/FacturaTallerMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturaTallerMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4970d3bfdb326cc1f5e4cc18bf14057282e3e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1419e482bcf7903f6c25425e2118f294bfd794")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturaTallerMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturaTallerMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
