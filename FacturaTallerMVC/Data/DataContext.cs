c96c4e54a27e32f28c67d4af2d1fd0ec5b7a2229
avazquez <pallarete@gmail.com>
2024-10-21 16:48:46 +0200

    fsdfsdfsdfs

--- FacturaTallerMVC/Data/DataContext.cs
+++ FacturaTallerMVC/Data/DataContext.cs
@@ -8,7 +8,7 @@ namespace FacturaTallerMVC.Data
 
         }
         //Sintaxis indiferente del motor de base de datos
-        public DbSet<Models.OrdenTrabajo> Facturas { get; set; }
+        public DbSet<Models.Factura> Facturas { get; set; }
         public DbSet<Models.Cliente> Clientes { get; set; }
         public DbSet<Models.Recambio> Recambios { get; set; }
         public DbSet<Models.Coche> Coches { get; set; }
